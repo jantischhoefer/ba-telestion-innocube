4e9c5682c8675e95f7519d6d9b15942587cfc2de
Jan Tischhöfer <jan.tischhoefer@gmail.com>
2022-04-06 11:42:05 +0200

    feat(client): Initialize client using tc-cli

---
+++ client/src/model/sample-user-config.ts
@@ -0,0 +1,51 @@
+import { UserConfig } from "@wuespace/telestion-client-types";
+
+export const userConfig: UserConfig = {
+	'admin': {
+		dashboards: [
+			{
+				title: 'Overview',
+				columns: 4,
+				rows: 4,
+				widgets: [
+					{
+						id: '0',
+						widgetName: 'sampleWidget',
+						width: 4,
+						height: 1
+					},
+					{
+						id: '1',
+						widgetName: 'Widget2',
+						width: 2,
+						height: 2
+					},
+					{
+						id: '2',
+						widgetName: 'Widget3',
+						width: 2,
+						height: 1
+					},
+					{
+						id: '3',
+						widgetName: 'Widget4',
+						width: 1,
+						height: 1
+					},
+					{
+						id: '4',
+						widgetName: 'Widget5',
+						width: 1,
+						height: 2
+					},
+					{
+						id: '5',
+						widgetName: 'Widget6',
+						width: 1,
+						height: 3
+					},
+				]
+			}
+		]
+	}
+};
